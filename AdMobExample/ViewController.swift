a7c357377dbb6b209d1abf6c1322bca0839a8ee9
ergeakova <ergeak@gmail.com>
2022-07-11 23:11:57 +0300

    added AdMob SDK

--- AdMobExample/ViewController.swift
+++ AdMobExample/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func show(_ sender: Any) {
+        print("Clicked")
+    }
+    
 }
 
